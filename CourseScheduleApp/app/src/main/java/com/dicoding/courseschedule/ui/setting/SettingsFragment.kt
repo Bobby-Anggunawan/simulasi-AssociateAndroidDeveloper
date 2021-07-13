174d0745dc62f0830aec6009d1ac7a0ef75ba6f6
Bobby-Anggunawan <yeremia.blog@gmail.com>
2021-07-14 04:33:19 +0700

    TODO 10

--- CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -1,15 +1,32 @@
 package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.util.NightMode
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat()  {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        //TODO 10 : Update theme based on value in ListPreference
+        //TODO 10(DONE) : Update theme based on value in ListPreference
+        val switchDarkMode: ListPreference? = findPreference(getString(R.string.pref_key_dark))
+        switchDarkMode?.setOnPreferenceChangeListener{ preference, newValue ->
+            val stringValue = newValue.toString()
+            if(stringValue == getString(R.string.pref_dark_auto)){
+                if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) updateTheme(NightMode.AUTO.value)
+                else updateTheme(NightMode.OFF.value)
+            }
+            else if(stringValue == getString(R.string.pref_dark_off)) updateTheme(NightMode.OFF.value)
+            else updateTheme(NightMode.ON.value)
+
+            true
+        }
         //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
     }
 
